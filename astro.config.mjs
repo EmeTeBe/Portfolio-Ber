802b9fc396a8e323bd5d59e3bf8577230049f5f2
EmeTeBe <bernimarozzi@gmail.com>
2025-03-30 22:23:38 -0300

    arreglo del seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+
+  output: 'static',
+  site: 'https://portfolio-ber.vercel.app/',
+  prefetch: false,
+
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
